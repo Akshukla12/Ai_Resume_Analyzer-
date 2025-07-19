be60d3c559c445c7cb811eaa2fd88f1d06abaec0
Akash <akashshukla2311@gmail.com>
2025-07-19 17:10:06 +0530

    Add deployment configuration and build files

--- server.js
+++ server.js
@@ -197,6 +197,11 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
+} else {
+  // Development route
+  app.get('/', (req, res) => {
+    res.json({ message: 'AI Resume Analyzer API is running' });
+  });
 }
 
 app.listen(PORT, () => {
